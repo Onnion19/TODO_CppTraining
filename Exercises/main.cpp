c4b8e68026f1f6d3e56412f92c1915f0a0de50d0
Aleix Rius <u1940025@campus.udg.edu>
2021-03-12 18:27:23 +0100

    Acualizar ejemplo

--- Exercises/main.cpp
+++ Exercises/main.cpp
@@ -1,5 +1,6 @@
 #include "Timer.h"
 #include "Header/Exercice1/Player.h"
+#include <iostream>
 
 
 /**
@@ -7,47 +8,56 @@
  * Tu objetivo es hacer una implementacion en la que el jugador se pueda equipar distintas armas,
  * y cuando decida atacar se ejecute una cosa u otra en funcion de la arma que lleve el jugador. 
  * 
- * Para simplificar el proceso, se debera llamar una funcion 'Atcar()' (o cualquiera que tu quieras) y debera imprimir por pantalla 
+ * Para simplificar el proceso, se debera llamar una funcion 'Atacar()' (o cualquiera que tu quieras) y debera imprimir por pantalla 
  * que tipo de arma esta usando el jugador. 
  * 
  * Restricciones: 
  * - Dificultad Baja: Ninguna.
  * - Dificultad Media: Debes usar COMO MINIMO la classe Player que encontraras dentro de Header/Exercice1/Player.h y Source/Exercice1/Player.cpp
  * - Dificultad Alta: Debes usar las clases que quieras, pero no puedes usar virtual.
- * 
+ * - Dificultad Nightmare: Debes usar templates
  */
 
+int Ejemplo(); //Se puede eliminar
 
 int main()
 {
 
-	Utils::Timer chrono("Tu Codigo"); //No eliminar o alterar esta linea
+	Utils::Timer chrono("Application"); //No eliminar o alterar esta linea
 
 	//Tu codigo va aqui
-
+	Ejemplo();
 	//
 
 	return 0;
 }
 
 //Ejemplo de una implementacion muy basica (super basica)
+//Todo el codigo de ejemplo se puede eliminar
 
-int Ejemplo {
-	
+void Atacar(int i)
+{
+	if (i == 0)
+		std::cout << "usando Espada" << std::endl;
+	else if (i == 1)
+		std::cout << "usando arco"<< std::endl;
+	else if (i == 2)
+		std::cout << "usando pistola"<< std::endl;
+}
+
+int Ejemplo ()
+{
+	//Indice al tipo de arma 
 	int i = 0; 
-	std::cout << "entra un numero de 0 a 2 para escoger una arma, o menor que 0 para salir" << std::endl; 
-	std::cin >> i; 
-	while (i >= 0) 
-	{
-		if ( i == 0) 
-			std::cout<< "usando Espada"; 
-		if(i==1)
-			std::cout << "usando arco"; 
-		if (i==2)
-			std::cout<<"usando pistola";
-		
-		std::cout << "entra un numero de 0 a 2, o menor que 0 para salir" << std::endl; 
-		std::cin >> i; 
+	//Por cada tipo de arma
+	while (i < 3) 
+	{	
+		//Atacar con el arma en concreto
+		Atacar(i);
+		//cambiar a la siguiente arma
+		i++;
 	}
+
+	return 0;
 		
 }
