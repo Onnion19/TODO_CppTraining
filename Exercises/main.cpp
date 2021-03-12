0c0ccf02446105c6b720b8c1c22bfe5d8330140d
Aleix Rius <aleixrius19@gmail.com>
2021-03-12 18:06:41 +0100

    Update main.cpp

--- Exercises/main.cpp
+++ Exercises/main.cpp
@@ -4,15 +4,16 @@
 
 /**
  * En este ejercicio eres un desarrollador que quiere que su jugador pueda llevar armas de distintos tipos: pistola , espada, arco. 
- * Tu objetivo es hacer una implementacion en la que se ejecute una cosa u otra en funcion de la arma que lleve. 
+ * Tu objetivo es hacer una implementacion en la que el jugador se pueda equipar distintas armas,
+ * y cuando decida atacar se ejecute una cosa u otra en funcion de la arma que lleve el jugador. 
  * 
- * Para simplificar, cuando se llame la funcion de EjecutarArma() (o cualquiera que tu quieras) debera imprimir por pantalla 
- * que tipo de arma es. 
+ * Para simplificar el proceso, se debera llamar una funcion 'Atcar()' (o cualquiera que tu quieras) y debera imprimir por pantalla 
+ * que tipo de arma esta usando el jugador. 
  * 
  * Restricciones: 
- * - Dificultad Baja: Ninguna, puedes usar todo lo que quieras y como quieras. 
- * - Dificultad Media: Debes usar la classe Player que encontraras dentro de Header/Exercice1/Player.h y Source/Exercice1/Player.cpp.
- * - Dificultad Alta: Debes usar la clase que quieras, pero no puedes usar virtual y aplicar el principio SOLID.
+ * - Dificultad Baja: Ninguna.
+ * - Dificultad Media: Debes usar COMO MINIMO la classe Player que encontraras dentro de Header/Exercice1/Player.h y Source/Exercice1/Player.cpp
+ * - Dificultad Alta: Debes usar las clases que quieras, pero no puedes usar virtual.
  * 
  */
 
@@ -27,4 +28,26 @@ int main()
 	//
 
 	return 0;
+}
+
+//Ejemplo de una implementacion muy basica (super basica)
+
+int Ejemplo {
+	
+	int i = 0; 
+	std::cout << "entra un numero de 0 a 2 para escoger una arma, o menor que 0 para salir" << std::endl; 
+	std::cin >> i; 
+	while (i >= 0) 
+	{
+		if ( i == 0) 
+			std::cout<< "usando Espada"; 
+		if(i==1)
+			std::cout << "usando arco"; 
+		if (i==2)
+			std::cout<<"usando pistola";
+		
+		std::cout << "entra un numero de 0 a 2, o menor que 0 para salir" << std::endl; 
+		std::cin >> i; 
+	}
+		
 }
